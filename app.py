c22e2cd67ad8a5c3db7c73c1d7efcbd423020a0b
scavenOP <niladris12345@gmail.com>
2021-08-29 12:24:50 +0530

    e

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ cam = cv2.VideoCapture(0)
 
 while run:
     ret, frame = cam.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     # result= detect_faces_live(frame)
     FRAME_WINDOW.image(frame)
 else:
